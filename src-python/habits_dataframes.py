b70242249af1853ee5041b45ea575e6cf61e48b4
leoscarton <lfscarton@gmail.com>
2026-03-15 11:07:50 -0300

    Refactored habit classes

--- src-python/habits_dataframes.py
+++ src-python/habits_dataframes.py
@@ -1,25 +1,7 @@
 import pandas as pd
 import numpy as np
 
-class Habit():
-    def __init__(self):
-        self._habit_columns = ['Name', 'Group', 'Frequency (Number)', 'Frequency (Type)', 'Instances', 'Current Streak', 'Maximum Streak']
-        self._habits = pd.DataFrame(columns=self._habit_columns)
-
-    def add_habit(self, new_habit:dict):
-        if new_habit['Name'] in self._habits['Name']:
-            raise AssertionError('Habit already exists.')
-        else:
-            new_habit_df = pd.DataFrame([new_habit])
-            new_habit_df = new_habit_df.reindex(columns=self._habit_columns)
-            new_habit_df["Instances"] = new_habit_df["Instances"].fillna(0).astype(int)
-
-            self._habits = pd.concat([self._habits, new_habit_df], ignore_index=True, axis=0)
-    
-    def remove_habit(self, habit_to_remove:str):
-        self._habits.drop([self._habits['Name'] == habit_to_remove], axis=0, inplace=True)
-        self._habits.reset_index()
-
+'''
 class HabitInstance():
     def __init__(self):
         self._habit_instance_columns = ['Name', 'Group', 'Date', 'Status']
@@ -36,7 +18,37 @@ class HabitInstance():
     def remove_habit_instance(self, instance_to_remove):
         self._habit_instances.drop([(self._habit_instances['Name'] == instance_to_remove['Name']) & (self._habit_instances['Date'] == instance_to_remove['Date'])])
         self._habit_instances.reset_index()
+'''
 
+class Habit():
+    def __init__(self, new_habit:dict):
+        self._habit_attributes = ['Name', 'Group', 'Frequency (Number)', 'Frequency (Type)', 'Instances', 'Current Streak', 'Maximum Streak']
+
+        self._habit_data = {k: new_habit.get(k) for k in self._habit_attributes}
+
+        #self._habit_instance_attributes = ['Name', 'Group', 'Date', 'Status']
+        self._habit_log_attributes = ['Date', 'Status']
+        
+
+    def autofill_incomplete_habit(self):
+        for k in self._habit_data.keys():
+            if k not in ['Name', 'Group', 'Frequency (Type)']:
+                self._habit_data[k] = 0
+
+    '''
+    def add_habit_log(self, new_instance:dict):
+        new_instance_df = pd.DataFrame([new_instance])
+        new_instance_df = new_instance_df.reindex(columns=self._habit_instance_attributes)
+
+        new_instance_df['Status'] = new_instance_df['Status'].fillna(False)
+
+        self._habit_instances = pd.concat([self._habit_instances, new_instance_df], ignore_index=True, axis=0)
+    
+        
+    def remove_habit(self, habit_to_remove:str):
+        self._habits.drop([self._habits['Name'] == habit_to_remove], axis=0, inplace=True)
+        self._habits.reset_index()
+    '''
 '''
 class EnvironmentConditions():
     def __init__(self):
@@ -46,3 +58,17 @@ class EnvironmentConditions():
             "Other" : []
         }
 '''
+
+class HabitManager():
+    def __init__(self):
+        self._habit_groups = {}
+
+    def add_new_habit(self, new_habit:dict, group:str):
+        habit = Habit(new_habit=new_habit)
+        
+        self._habit_groups.setdefault(group, {})
+        self._habit_groups[group].setdefault(habit['Name'], habit)
+
+    def remove_habit(self, habit_name:str, habit_group:str):
+        if habit_group in self._habit_groups.keys():
+            self._habit_groups[habit_group].pop(habit_name, None)
