87f0c10e89715e501daff2e86b4aed5b6d47aa1e
leoscarton <lfscarton@gmail.com>
2026-03-15 02:37:30 -0300

    Restarted Python backend

---
+++ src-python/habits_dataframes.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+
+class HabitDataFrame():
+    def __init__(self):
+        self._habit_columns = ['Name', 'Group', 'Frequency (Number)', 'Frequency (Type)', 'Instances']
+        self._habits = pd.DataFrame(columns=self._habit_columns)
+
+    def add_habit(self, new_habit:dict):
+        new_habit_df = pd.DataFrame([new_habit])
+        new_habit_df = new_habit_df.reindex(columns=self._habit_columns)
+        new_habit_df["Instances"] = new_habit_df["Instances"].fillna(0).astype(int)
+
+        self._habits = pd.concat([self._habits, new_habit_df], ignore_index=True, axis=0)
+
+class HabitInstanceDataFrame():
+    def __init__(self):
+        self._habit_instance_columns = ['Name', 'Group', 'Date', 'Status']
+        self._habit_instances = pd.DataFrame(columns=self._habit_instance_columns)
+
+    def add_habit_instance(self, new_instance:dict):
+        new_instance_df = pd.DataFrame([new_instance])
+        new_instance_df = new_instance_df.reindex(columns=self._habit_instance_columns)
+
+        new_instance_df['Status'] = new_instance_df['Status'].fillna(False)
+
+        self._habit_instances = pd.concat([self._habit_instances, new_instance_df], ignore_index=True, axis=0)
